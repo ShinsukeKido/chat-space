d6a7ad59f0ef2847ae303101eb2bc059c4d34c0c
城戸　真祐 <downmap555@gmail.com>
2018-01-05 18:57:40 +0900

    Fix3 after mentor check

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,6 +25,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { user_ids: [] })
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
